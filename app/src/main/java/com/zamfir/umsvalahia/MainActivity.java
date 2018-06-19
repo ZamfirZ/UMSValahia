ce297d7213ddd248454855352ea018e1b26cdaa4
Zamfir <edizamfir12@gmail.com>
2018-06-19 22:24:50 +0300

    2

--- app/src/main/java/com/zamfir/umsvalahia/MainActivity.java
+++ app/src/main/java/com/zamfir/umsvalahia/MainActivity.java
@@ -10,17 +10,15 @@ import android.webkit.WebSettings.LayoutAlgorithm;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
-
-import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 import com.google.firebase.analytics.FirebaseAnalytics;
 
 public class MainActivity extends AppCompatActivity{
 
     private FirebaseAnalytics mFirebaseAnalytics;
     private WebView myWebView;
-    private static final String TAG = "MainActivity";
     //......................................
     private AdView mAdView;
     //......................................
@@ -39,27 +37,28 @@ public class MainActivity extends AppCompatActivity{
         webSettings.setLayoutAlgorithm(LayoutAlgorithm.SINGLE_COLUMN);
         myWebView.getSettings().setLayoutAlgorithm(LayoutAlgorithm.SINGLE_COLUMN);
         myWebView.setOnTouchListener(new WebViewTouchListener());
+
+
+
+
+        myWebView.getSettings().setJavaScriptEnabled(true);
         //.......................................................................................
+        // Initialize the Mobile Ads SDK.
+        MobileAds.initialize(this, "ca-app-pub-4878467226249861~8776848389");
+
+        // Gets the ad view defined in layout/ad_fragment.xml with ad unit ID set in
+        // values/strings.xml.
         mAdView = findViewById(R.id.adView);
-        AdRequest adRequest = new AdRequest.Builder().build();
+
+        // Create an ad request. Check your logcat output for the hashed device ID to
+        // get test ads on a physical device. e.g.
+        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+
+        // Start loading the ad in the background.
         mAdView.loadAd(adRequest);
-        mAdView.setAdListener(new AdListener() {
-
-            @Override
-            public void onAdFailedToLoad(int errorCode) {
-                // Code to be executed when an ad request fails.
-                AdRequest adRequest = new AdRequest.Builder().build();
-                mAdView.loadAd(adRequest);
-            }
-
-            @Override
-            public void onAdClosed() {
-                // Code to be executed when when the user is about to return
-                // to the app after tapping on an ad.
-                AdRequest adRequest = new AdRequest.Builder().build();
-                mAdView.loadAd(adRequest);
-            }
-        });
 
 
 //..................................................................................................................
@@ -122,5 +121,31 @@ public class MainActivity extends AppCompatActivity{
             }
             return false;
         }
+    }
+    /** Called when leaving the activity */
+    @Override
+    public void onPause() {
+        if (mAdView != null) {
+            mAdView.pause();
+        }
+        super.onPause();
+    }
+
+    /** Called when returning to the activity */
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mAdView != null) {
+            mAdView.resume();
+        }
+    }
+
+    /** Called before the activity is destroyed */
+    @Override
+    public void onDestroy() {
+        if (mAdView != null) {
+            mAdView.destroy();
+        }
+        super.onDestroy();
     }
 }
